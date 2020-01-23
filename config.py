02334076516b249cee843b2c7823aee2d18bf30e
Amber Adams <ladyjacketamber@aol.com>
2020-01-22 23:19:36 -0500

    Setting up Function Outlines and Config for Walk of Fame Scrape

---
+++ config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 22:57:12 2020
+
+@author: Amber
+"""
+
+WIKIPEDIA_URL = "https://en.wikipedia.org"
+
+WALK_OF_FAME_URL = ("https://en.wikipedia.org/wiki/" 
+                    + "List_of_stars_on_the_Hollywood_Walk_of_Fame")
+
+OUTPUT_PATH = ("C:\\Users\\Ladyj\\Documents\\GitHub\\"
+               + "Walk-Of-Fame-Marriage-Analysis\\output")
+
+NAMES_OUTPUT = OUTPUT_PATH + "\\walk_of_fame_names.csv"
---
+++ scrape_walk_of_fame.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 22:59:28 2020
+
+@author: Amber
+"""
+import requests
+import pandas as pd
+import config
+from bs4 import BeautifulSoup
+
+def scrape_walk_of_fame():
+    pass
+
+def get_soup(link):
+    pass
+
+def extract_links(soup):
+    pass
+
+def get_star_table(soup):
+    pass
+
+def html_to_pandas(soup_text):
+    pass
+
+def add_links_to_table(table, links):
+    pass
